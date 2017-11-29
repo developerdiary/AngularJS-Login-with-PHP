5661bd0fdfabc59b47f0012bfd069f24012223d5
Developer's Diary <30465804+developerdiary@users.noreply.github.com>
2017-11-29 16:10:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AngularJS-Login-with-PHP
-My First Try Angular JS login with PHP 
+My First Try Angular JS login using PHP and MYSQL
---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login with PHP</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    </head>
+
+    <body ng-app="AngularJSLogin" ng-controller="AngularLoginController as angCtrl">
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container">
+            <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                <div class="panel panel-info" >
+                        <div class="panel-heading">
+                            <div class="panel-title">Angularjs Login Script using PHP MySQL and Bootstrap</div>
+      
+                        </div>
+      
+                        <div style="padding-top:30px" class="panel-body" >
+                            <form name="login" ng-submit="angCtrl.loginForm()" class="form-horizontal" method="POST">
+      
+                                <div style="margin-bottom: 25px" class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                    <input type="email" id="inputemail" class="form-control" required autofocus ng-model="angCtrl.inputData.email">
+                                </div>
+      
+                                <div style="margin-bottom: 25px" class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input type="password" id="inputpassword" class="form-control" required ng-model="angCtrl.inputData.password">
+                                </div>
+                                <div class="form-group">
+                                    <!-- Button -->
+                                    <div class="col-sm-12 controls">
+                                        <button type="submit" class="btn btn-primary pull-left"><i class="glyphicon glyphicon-log-in"></i> Log in</button>
+                                    </div>
+                                </div>
+                                    <div class="alert alert-danger" ng-show="errorMsg">
+                                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                                            ×</button>
+                                        <span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;{{errorMsg}}
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+            </div>
+        </div>
+        
+
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular.min.js"></script>
+
+
+        <script>
+
+
+            angular.module('AngularJSLogin', [])
+            .controller('AngularLoginController', function($scope, $http){
+                this.loginForm = function(){
+                    var user_data='user_email='+this.inputData.email+'&user_password='+this.inputData.password;
+                    $http({
+                        method : "POST",
+                        url : "login.php",
+                        data: user_data,
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+
+                    }).then(function mySuccess(response) {
+                       console.log(response.data);
+                        if ( response.data === 'correct') {
+                            window.location.href = 'welcome_dashboard.php';
+                        } else {
+                            $scope.errorMsg = "Invalid Email and Password";
+                        }
+                    }, function myError(response) {
+                        console.log('Error Handle');
+                    });                    
+                }
+            });
+
+        </script>
+
+    </body>
+</html>
